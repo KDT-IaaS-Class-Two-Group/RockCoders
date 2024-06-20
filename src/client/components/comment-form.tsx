e9227f71c336856d8ba4337060346471acf7ce83
eonduck2 <whdtnxd@gmail.com>
2024-06-20 14:46:52 +0900

    :sparkles: 방명록 틀 생성

--- src/client/components/comment-form.tsx
+++ src/client/components/comment-form.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-
 /**
  * @eonduck2 24.06.20
  * * 방명록 틀이 될 폼 컴포넌트
  */
-const commentForm = () => {};
+const commentForm: React.FC = () => {
+  return (
+    <div>
+      <form action="/comment">
+        <input type="text" />
+        <input type="text" />
+        <input type="submit" name="" id="" />
+      </form>
+    </div>
+  );
+};
 
 export default commentForm;
