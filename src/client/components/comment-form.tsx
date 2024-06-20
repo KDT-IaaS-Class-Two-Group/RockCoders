87f391f9ddfdad76ee91a606248f441e5eababff
eonduck2 <whdtnxd@gmail.com>
2024-06-20 14:34:41 +0900

    :memo: 폼 컴포넌트 함수 정의

--- src/client/components/comment-form.tsx
+++ src/client/components/comment-form.tsx
@@ -1,14 +1,5 @@
 import React from "react";
 
-const commentForm = () => {
-  return (
-    <div>
-      <form action="/comment">
-        <input type="text" name="nickname" />
-        <input type="text" name="content" />
-      </form>
-    </div>
-  );
-};
+const commentForm = () => {};
 
 export default commentForm;
