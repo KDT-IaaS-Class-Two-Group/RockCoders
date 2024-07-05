024b5b69e5a6e3254772ac47c6e9631f2deab889
eonduck2 <whdtnxd@gmail.com>
2024-07-05 17:06:30 +0900

    :pacakge: 이종수: 리액트 기본 컴포넌트 정의

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import "../styles/input.css";
 const App: React.FC = () => {
-  return <div>테스트</div>;
+  return (
+    <>
+      <div>기본 템플릿입니다.</div>
+    </>
+  );
 };
 
 export default App;
--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,22 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
 
-// * 리액트 18 버전 이상 렌더링 방식
-createRoot(document.getElementById("root")!).render(
+const root = document.getElementById(`root`) as HTMLElement;
+
+createRoot(root).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// ! 리액트 17 버전 렌더링 방식(18 버전에서 지원 X)
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById("root")
-// );
-// ! ----------------------------------
-
-// const container = document.getElementById('app');
-// const root = createRoot(container); // createRoot(container!) if you use TypeScript
-// root.render(<App tab="home" />);
