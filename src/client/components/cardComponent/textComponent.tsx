9dfe478aae49ac3c6e0ff470ea13d76c259474d7
moonhr <moonhr153@gmail.com>
2024-07-05 18:02:25 +0900

    :memo:문혜림:주석추가

--- src/client/components/cardComponent/textComponent.tsx
+++ src/client/components/cardComponent/textComponent.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 
+//textComponent 함수의 interface 정의
 interface TextComponentProps {
     text: string;
     className?: string;}
 
+/**
+ * @moonhr 24.07.05
+ * * 카드 제작에 사용될 기본 textComponent
+ * @param text="" className="" 으로 작성하면 됨.
+ * @returns p태그
+ */
 const TextComponent: React.FC<TextComponentProps> = ({ text, className = '' }) => {
     // 기본 TailwindCSS 클래스
     const defaultClasses = 'text-base font-normal text-black';
 
-    //스프레드 연산자 (...)로 오른쪽 객체의 속성이 왼쪽 객체의 속성을 덮어쓴다.(객체 스프레드)
+    //클래스 병합
     const combinedClasses = `${defaultClasses} ${className}`;
 
     return <p className={combinedClasses}>{text}</p>;
