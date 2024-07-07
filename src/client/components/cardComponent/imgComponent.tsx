12ba7843184d091ae465b2caed73e3239f7a3779
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-07 17:03:38 +0900

    :bulb:유호영:img컴포넌트 주석 추가

--- src/client/components/cardComponent/imgComponent.tsx
+++ src/client/components/cardComponent/imgComponent.tsx
@@ -6,6 +6,14 @@ interface ImgComponentProps {
   className? : string
 }
 
+/**
+ * @crystal23733 24.07.07
+ * 카드 제작에 사용될 이미지
+ * @param imgUrl 이미지의 주소값
+ * @param alt 내용설명
+ * @param className 클래스 명
+ * @returns img태그
+ */
 const ImgComponent: React.FC<ImgComponentProps> = ({imgUrl,alt = '',className = ''}) => {
   return <img src={imgUrl} alt={alt} className={className} />;
 }
