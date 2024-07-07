2016a3bba63d921252d7630a1ee449853787f08f
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-07 17:52:40 +0900

    :sparkles:유호영:카드 컴포넌트 생성

---
+++ src/client/components/cardComponent/cardComponent.tsx
@@ -0,0 +1,22 @@
+import React, {ReactNode} from "react";
+
+interface CardComponentProps {
+  className? : string,
+  children? : ReactNode
+}
+
+/**
+ * @crystal23733 24.07.07
+ * @param className 클래스 스타일 
+ * @param children 자식요소들
+ * @returns div 카드 태그 생성
+ */
+const CardComponent:React.FC<CardComponentProps> = ({className = '', children}) => {
+  return (
+    <div className={`bg-white w-443 h-600 rounded-lg shadow-lg overflow-hidden ${className}`}>
+      {children}
+    </div>
+  )
+}
+
+export default CardComponent;
