7f817fa1b157957e549d29bd2175eccf8189b364
eonduck2 <whdtnxd@gmail.com>
2024-07-08 17:24:20 +0900

    :package: 이종수: 깨지는 이펙트 컴포넌트 기본 정의 완료

---
+++ src/client/components/intro/FrameBreaker.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import IFrameBreaker from "./FrameBreaker.interface";
+import StyledFrameBreaker from "./FrameBreaker.style";
+
+const FrameBreaker: React.FC<IFrameBreaker> = () => {
+  return <StyledFrameBreaker src=""></StyledFrameBreaker>;
+};
+
+export default FrameBreaker;
