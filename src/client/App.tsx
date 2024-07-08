7f63721544276b50b4d4a29348bf8535ea42027a
eonduck2 <whdtnxd@gmail.com>
2024-07-08 17:49:32 +0900

    :heavy_plus_sign: 이종수: 'file-loader' 패키지 추가 의존

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import FrameBreaker from "./components/intro/FrameBreaker";
 const App: React.FC = () => {
   return (
     <>
       <div>기본 템플릿입니다.</div>
+      <FrameBreaker />
     </>
   );
 };
