d6a3dc8babc8f66ef61c3d753698f3677f307eba
eonduck2 <whdtnxd@gmail.com>
2024-07-08 17:42:36 +0900

    :package: 이종수: 깨지는 화면 컴포넌트 기본 정의

--- src/client/components/intro/FrameBreaker.tsx
+++ src/client/components/intro/FrameBreaker.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import IFrameBreaker from "./FrameBreaker.interface";
 import StyledFrameBreaker from "./FrameBreaker.style";
+import FB from "../../../../public/assets/gifs/frame_breaker.gif";
 
 const FrameBreaker: React.FC<IFrameBreaker> = () => {
-  return <StyledFrameBreaker src="" />;
+  return <StyledFrameBreaker src={FB} />;
 };
 
 export default FrameBreaker;
