48d0f05017c5aba3972699ca55adb001c13129a8
moonhr <moonhr153@gmail.com>
2024-07-08 16:46:57 +0900

    :art:문혜림:style강제

--- src/client/components/cardComponent/cardBack/cardBackTitle.tsx
+++ src/client/components/cardComponent/cardBack/cardBackTitle.tsx
@@ -7,7 +7,7 @@ const CardBackTitle:React.FC = () => {
     <div className='grid grid-cols-2 grid-rows-3 '>
       <TextComponent text='Rock' className='font-black !text-7xl col-start-1' />
       <TextComponent text='Coders' className='font-black !text-7xl col-start-1' />
-      <TextComponent text='Personal Project' className='font-semibold text-5xl col-start-1' />
+      <TextComponent text='Personal Project' className='font-semibold !text-5xl col-start-1' />
       <ImgComponent imgUrl='../../../../Img/rock.png' alt='주먹' className='w-16 col-start-2 row-start-1 justify-self-end' />
     </div>
   );
