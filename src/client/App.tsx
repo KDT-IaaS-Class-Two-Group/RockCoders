f38d29854fb954fd1d416089a7ce359bacf0aa8b
Hoyeong Yoo <crystal23733@gmail.com>
2024-07-09 17:59:40 +0900

    :white_check_mark:유호영:기능 구현을 위해 app.tsx에 컴포넌트 할당

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import FrontCardComponent from "./components/cardComponent/Front/frontCardComponent";
 const App: React.FC = () => {
   return (
     <>
       <div>기본 템플릿입니다.</div>
+      <FrontCardComponent />
     </>
   );
 };
