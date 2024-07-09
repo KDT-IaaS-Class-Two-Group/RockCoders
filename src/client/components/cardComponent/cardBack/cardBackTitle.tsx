12400f2b9d0713824cb0d6185c3091f52e9fde10
moonhr <moonhr153@gmail.com>
2024-07-09 14:56:56 +0900

    :art:문혜림:text사이즈 변경

--- src/client/components/cardComponent/cardBack/cardBackTitle.tsx
+++ src/client/components/cardComponent/cardBack/cardBackTitle.tsx
@@ -5,9 +5,9 @@ import ImgComponent from '../imgComponent';
 const CardBackTitle:React.FC = () => {
   return (
     <div className='grid grid-cols-2 grid-rows-3 '>
-      <TextComponent text='Rock' className='font-black !text-7xl col-start-1' />
-      <TextComponent text='Coders' className='font-black !text-7xl col-start-1' />
-      <TextComponent text='Personal Project' className='font-semibold !text-5xl col-start-1' />
+      <TextComponent text='Rock' className='font-black !text-4xl col-start-1' />
+      <TextComponent text='Coders' className='font-black !text-4xl col-start-1' />
+      <TextComponent text='Personal Project' className='font-semibold !text-xl col-start-1' />
       <ImgComponent imgUrl='../../../../Img/rock.png' alt='주먹' className='w-16 col-start-2 row-start-1 justify-self-end' />
     </div>
   );
