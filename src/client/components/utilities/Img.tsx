09330116109d4bccf6aa7e838e2623356aae9c7c
eonduck2 <whdtnxd@gmail.com>
2024-07-10 14:41:53 +0900

    :bulb: 이종수: 이미지 컴포넌트 주석 추가

--- src/client/components/utilities/Img.tsx
+++ src/client/components/utilities/Img.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import TImg from "./Img.type";
 
+/**
+ * @eonduck2 24.07.10
+ * * 이미지 컴포넌트를 생성하기 위한 틀
+ * @param { string } src 파일 경로와 이름
+ * @param { string } extension 확장자
+ * @param { string } rest Image 태그가 가질 수 있는 지정된 속성들 ex) className = "some-class"
+ * @returns { JSX.Element } 이미지를 표시하는 img 엘리먼트
+ */
 const ImageComponent: React.FC<TImg> = ({ src, extension, ...rest }) => {
   const fullSrc = src + "." + extension;
+
   return <img src={fullSrc} {...rest} />;
 };
 
