aad082ad676737c5b1dec8991ec76caae9d64e76
eonduck2 <whdtnxd@gmail.com>
2024-07-10 15:33:54 +0900

    :bulb: 이종수: 이미지 컴포넌트 주석 수정

--- src/client/components/utilities/Img.tsx
+++ src/client/components/utilities/Img.tsx
@@ -4,8 +4,8 @@ import TImg from "./Img.type";
 /**
  * @eonduck2 24.07.10
  * * 이미지 컴포넌트를 생성하기 위한 틀
- * @param { string } src 파일 경로와 이름
- * @param { string } extension 확장자
+ * @param { string } src 파일 경로와 이름 || 확장자까지 갖고있는 전체 경로
+ * @param { string } extension ?.확장자
  * @param { string } rest Image 태그가 가질 수 있는 지정된 속성들 ex) className = "some-class"
  * @returns { JSX.Element } 이미지를 표시하는 img 엘리먼트
  */
