f5d1e9b4c5520ec273146657694fbe0bc118667c
eonduck2 <whdtnxd@gmail.com>
2024-07-10 14:46:33 +0900

    :package: 이종수: 깨지는 화면 정적 파일명 변경

---
+++ static/FrameBreakerObj.ts
@@ -0,0 +1,9 @@
+const FBPath: string = "./assets/gifs/frame_breaker.mov";
+
+const FrameBreakerObj: object = {
+  path: "./assets/gifs/",
+  fileName: "frame_breaker",
+  extension: "gif"
+};
+
+export default FrameBreakerObj;
--- static/FrameBreakerPath.ts
+++
@@ -1,3 +0,0 @@
-const FBPath: string = "./assets/gifs/frame_breaker.mov";
-
-export default FBPath;
