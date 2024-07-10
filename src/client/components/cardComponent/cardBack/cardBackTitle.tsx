4c978d124c07b067070558c5a1ea454f571f2002
moonhr <moonhr153@gmail.com>
2024-07-10 10:29:26 +0900

    :package:문혜림:classname수정을 위한 interface 추가

--- src/client/components/cardComponent/cardBack/cardBackTitle.tsx
+++ src/client/components/cardComponent/cardBack/cardBackTitle.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import TextComponent from '../textComponent';
 import ImgComponent from '../imgComponent';
 
-const CardBackTitle:React.FC = () => {
+interface CardBackTitle {
+  className?: string;
+}
+
+const CardBackTitle:React.FC<CardBackTitle> = ({className}) => {
   return (
-    <div className='grid grid-cols-2 grid-rows-3 '>
-      <TextComponent text='Rock' className='font-black !text-4xl col-start-1' />
-      <TextComponent text='Coders' className='font-black !text-4xl col-start-1' />
+    <div className={`grid grid-cols-2 grid-rows-3 ${className}`}>
+      <TextComponent text='Rock' className='!font-black !text-5xl col-start-1' />
+      <TextComponent text='Coders' className='!font-black !text-5xl col-start-1' />
       <TextComponent text='Personal Project' className='font-semibold !text-xl col-start-1' />
       <ImgComponent imgUrl='../../../../Img/rock.png' alt='주먹' className='w-16 col-start-2 row-start-1 justify-self-end' />
     </div>
