7ace1be414ba03101f01d5f2890c6e7fc1583e6d
moonhr <moonhr153@gmail.com>
2024-07-10 10:28:37 +0900

    :package:문혜림:classname수정을 위한 interface추가

--- src/client/components/cardComponent/cardBack/cardBackBody.tsx
+++ src/client/components/cardComponent/cardBack/cardBackBody.tsx
@@ -3,15 +3,21 @@ import TextComponent from '../textComponent';
 import ProgressBar from './progressBar';
 import CardBackBodyProps from './interface/CardBackBodyProps';
 
+interface CardBackBodyComponentProps {
+  className?: string;
+  props: CardBackBodyProps;
+}
+
 /**
  * @moonhr 24.07.09
- * @param CardBackBodyProps
+ * @param className
+ * @param 개인프로젝트 객체
  * @returns 개인프로젝트 부분
  */
-const CardBackBody:React.FC<CardBackBodyProps> = (props) => {
+const CardBackBody:React.FC<CardBackBodyComponentProps> = ({className, props}) => {
   return (
-    <div className='flex flex-col'>
-      <TextComponent text={props.mainText} className='text-3xl' />
+    <div className={`flex flex-col ${className}`}>
+      <TextComponent text={props.mainText} className='!text-2xl !font-semibold' />
       <TextComponent text={props.bodyText} className='text-xs	' />
       <a href={props.githubLink} className='text-sm	font-semibold'>GitHub Link</a>
       <ProgressBar progress={props.progressPercentage} />
