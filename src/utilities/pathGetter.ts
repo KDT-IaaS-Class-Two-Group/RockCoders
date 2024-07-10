381de3ff8a7949fdcbc08b58ccf9a013e81872d6
eonduck2 <whdtnxd@gmail.com>
2024-07-10 15:00:45 +0900

    :bulb: 이종수: 경로 합치는 모듈 주석 지정

--- src/utilities/pathGetter.ts
+++ src/utilities/pathGetter.ts
@@ -1,3 +1,8 @@
 import path from "node:path";
 
+/**
+ * @eonduck2 24.07.10
+ * * 지정된 경로를 합쳐주는 모듈
+ * @param { string } paths 문자열 형식의 리터럴 eg) "test1", "test2", "test3", ...
+ */
 export default (...paths: string[]) => path.resolve(...paths);
